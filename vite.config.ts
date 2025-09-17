ed4aab34f605504ea65963f16ac2367ead908166
ControladoriaGen <Gustavo.pimenta@generoso.com.br>
2025-09-17 14:07:34 -0300

    init: analitico-cdi

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/AnaliticoCDI/',   // <<-- nome EXATO do repositório no GitHub
+  base: '/analitico-cdi/',   // <<-- nome EXATO do repositório no GitHub
 })
